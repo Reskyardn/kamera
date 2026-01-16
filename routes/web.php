40de6ef0fbdb692bdcafca2ac385eb3cf131c6c7
Reskayardn <reskyardiani22@gmail.com>
2026-01-16 16:54:39 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Auth Routes (Guest only)
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
